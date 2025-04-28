39c4df4c02a4607f8e3e0d375896914169b844ed
Jswears <ji.swearssalinas@gmail.com>
2025-04-28 14:44:58 +0200

    feat(metrics): push custom cloudwatch metrics: temperature, humidity, apitimeelapsed

--- lambda_function/utils.py
+++ lambda_function/utils.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 import requests
+import boto3
 
 def get_weather_data(city):
     load_dotenv()
@@ -26,3 +27,42 @@ def get_weather_data(city):
     else:
         return None
     
+# Function to push metrics to CloudWatch
+def push_cloudwatch_metrics(city, temperature_celsius, humidity_percent, elapsed_time):
+    cloudwatch = boto3.client('cloudwatch')
+    
+    # Push custom metrics to CloudWatch
+    cloudwatch.put_metric_data(
+        Namespace='WeatherMetrics',
+        MetricData=[
+            {
+                'MetricName': 'Temperature',
+                'Dimensions': [
+                    {
+                        'Name': 'City',
+                        'Value': city
+                    },
+                ],
+                'Value': temperature_celsius,
+                'Unit': 'None'
+            },
+            {
+                'MetricName': 'Humidity',
+                'Dimensions': [
+                    {
+                        'Name': 'City',
+                        'Value': city
+                    },
+                ],
+                'Value': humidity_percent,
+                'Unit': 'None'
+            },
+            {
+                'MetricName': 'APIResponseTime',
+                'Dimensions': [{'Name': 'City', 'Value': city}],
+                'Unit': 'Milliseconds',
+                'Value': elapsed_time
+            }
+        ]
+    )
+    print(f"Metrics pushed to CloudWatch for {city}: Temperature={temperature_celsius}, Humidity={humidity_percent}")    
