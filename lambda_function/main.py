8d9c65b53eead4cd9808dca40b46622ecefd82f9
Jswears <ji.swearssalinas@gmail.com>
2025-04-28 14:38:21 +0200

    feat(weather): Fetch weather data from API

---
+++ lambda_function/main.py
@@ -0,0 +1,31 @@
+from utils import get_weather_data
+
+# Lambda function handler
+def lambda_handler(event, context):
+    city_name = event.get('city', 'Hamburg')
+    weather_data = get_weather_data(city_name)
+    
+    if weather_data:
+        city = weather_data['city']
+        temperature = weather_data['temperature']
+        humidity_percent = weather_data['humidity_percent']
+        description = weather_data['description']
+        elapsed_time = weather_data['elapsed_time']
+        
+        return {
+            'statusCode': 200,
+            'body': {
+                'city': city,
+                'temperature': temperature,
+                'humidity_percent': humidity_percent,
+                'description': description,
+                'elapsed_time': elapsed_time                
+            }
+        }
+    else:
+        return {
+            'statusCode': 500,
+            'body': {
+                'error': 'Could not retrieve weather data'
+            }
+        }
