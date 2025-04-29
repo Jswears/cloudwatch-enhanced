d40018c96859e0418d2250f885ed4fdeea9268ad
Jswears <ji.swearssalinas@gmail.com>
2025-04-29 08:20:30 +0200

    infra(lambda): variables, iam roles/policies, lambda and outputs config

---
+++ terraform/lambda.tf
@@ -0,0 +1,10 @@
+resource "aws_lambda_function" "cloudwatch_enhanced" {
+    filename         = "${path.module}/../lambda_function.zip"
+    function_name    = var.lambda_function_name
+    role             = aws_iam_role.lambda_exec_role.arn
+    handler          = var.lambda_handler
+    runtime          = var.lambda_runtime
+    memory_size     = var.lambda_memory_size
+    timeout = var.lambda_timeout
+    source_code_hash = filebase64sha256("${path.module}/../lambda_function.zip")
+}
